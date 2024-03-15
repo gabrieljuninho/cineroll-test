f99385328802f38a41d6cd60c3a23c754a3ba1c4
gabrieljuninho <gabrieltobing789@gmail.com>
2024-03-15 20:16:09 +0700

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, { Application, Request, Response } from "express";
+import dotenv from "dotenv";
+
+const app: Application = express();
+
+dotenv.config();
+
+const PORT: Number = Number(process.env.SERVER_PORT);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello world");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
